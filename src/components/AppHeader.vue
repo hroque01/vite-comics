ca34aa433d93647a54b296a2f2b0c550abf1c17c
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-14 19:03:09 +0800

    Addd nav bar to template and remove to array

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,41 +2,6 @@
 export default {
     data() {
         name: 'AppHeader'
-        navMenu: [
-            {
-                navigation: "CHARACTERS"
-            },
-            {
-                navigation: "COMICS"
-            },
-            {
-                navigation: "MOVIES"
-            },
-            {
-                navigation: "TV"
-            },
-            {
-                navigation: "GAMES"
-            },
-            {
-                navigation: "CHARACTERS"
-            },
-            {
-                navigation: "COLLECTIBLES"
-            },
-            {
-                navigation: "VIDEOS"
-            },
-            {
-                navigation: "FANS"
-            },
-            {
-                navigation: "NEWS"
-            },
-            {
-                navigation: "SHOP"
-            }
-        ]
     }
 }
 </script>
@@ -45,13 +10,70 @@ export default {
 
     <header>
         <a href="#">
-            <img src="/dc-logo-bg.png" alt="">
+            <img src="/dc-logo.png" alt="">
         </a>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        CHARACTERS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        COMICS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        MOVIES
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TV
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        GAMES
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        COLLECTIBLES
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        VIDEOS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        FANS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        NEWS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        SHOP
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </header>
 
 </template>
 
 <style lang="scss" scoped>
+@use '../temp/partials/mixins.scss';
+@use '../temp/partials/variables.scss';
+
 header {
     text-align: center;
 }
