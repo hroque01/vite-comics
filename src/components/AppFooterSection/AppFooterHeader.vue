6511c2a7cdd22f9cd4a913372f611cd72931470a
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-14 21:31:50 +0800

    Create section of footer

---
+++ src/components/AppFooterSection/AppFooterHeader.vue
@@ -0,0 +1,12 @@
+<script>
+name: "TopFooter"
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../temp/partials/mixins.scss' as *;
+@use '../temp/partials/variables.scss' as *;
+</style>
